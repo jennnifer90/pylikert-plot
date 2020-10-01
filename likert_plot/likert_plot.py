4de93eac6a8eee2157b7f393cba04e26018537e6
jennnifer90 <jennnifer90@hotmail.com>
2020-10-01 16:20:09 -0400

    added color and order

--- likert_plot/likert_plot.py
+++ likert_plot/likert_plot.py
@@ -1,24 +1,28 @@
-from typing import Optional, Dict, Any
+from typing import Optional, Dict, Any, Union, List
 
 import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 from matplotlib import patches
+from matplotlib.colors import to_rgb
 from mpl_format.axes import AxesFormatter
 from mpl_format.axes.axis_utils import new_axes
 from pandas import DataFrame
 from seaborn import color_palette
 
 
-def plot_likert_scales(data: DataFrame, align_category: str,
-                       color_palette_name: Optional[str] = "RdBu",
-                       align_type: Optional[str] = 'center',
-                       bar_kws: Optional[Dict[str, Any]] = None,
-                       legend_kws: Optional[Dict[str, Any]] = None,
-                       line_at_zero: Optional[bool] = True,
-                       show_perc: Optional[bool] = True,
-                       min_label_perc: Optional[float] = 3.0,
-                       title: Optional[str] = None,
-                       qname_mapping: Optional[Dict[str,str]] = None):
+def plot_likert_scales_temp(data: DataFrame, align_category: str,
+                            color_palette_name: Optional[str] = "RdBu",
+                            order: Optional[List[str]]= None,
+                            color: Optional[Union[Dict[str,str], List[str]]]
+                            = None,
+                            align_type: Optional[str] = 'center',
+                            bar_kws: Optional[Dict[str, Any]] = None,
+                            legend_kws: Optional[Dict[str, Any]] = None,
+                            line_at_zero: Optional[bool] = True,
+                            show_perc: Optional[bool] = True,
+                            min_label_perc: Optional[float] = 3.0,
+                            title: Optional[str] = None,
+                            qname_mapping: Optional[Dict[str, str]] = None):
     """
 
     :param data: DataFrame with counts of Likert choices as columns and
@@ -26,6 +30,8 @@ def plot_likert_scales(data: DataFrame, align_category: str,
     negative choice as the first column and most positive choice as the
     last column.
     :param align_category: Likert scale/choice name to be aligned at 0.
+    :param order: Order of the data columns
+    :param color: Dict or List to specify the color of the plot
     :param color_palette_name: name of seaborn color palette to plot likert
     bars
     :param align_type: 'center' of 'left'. Default is center meaning
@@ -43,6 +49,12 @@ def plot_likert_scales(data: DataFrame, align_category: str,
     name'}
     :return:
     """
+
+    if order is not None:
+        data = data[order]
+
+    elif color is None:
+        fc = color_palette(color_palette_name, n_colors=data.shape[1])
 
     if bar_kws is None:
         bar_kws = {'height': 0.8}
@@ -64,46 +76,73 @@ def plot_likert_scales(data: DataFrame, align_category: str,
     y_position = []
     y = len(data) * bar_kws['height'] + 0.5 - 0.5 * bar_kws['height'] + len(
         data)*0.2
-    color = color_palette(color_palette_name, n_colors=data.shape[1])
     ax = new_axes()
 
     for name, row in data.iterrows():
-        mid_index = data.columns.get_loc(align_category)
+        align_index = data.columns.get_loc(align_category)
         if align_type == 'center':
             mid_point = -row[align_category] / 2
         elif align_type == 'left':
             mid_point = 0
         else:
             raise ValueError('align type can only be center or left')
+
+        if color is not None:
+            if type(color) == dict:
+                fc_mid = color[align_category]
+                r,b,g = to_rgb(fc_mid)
+            elif type(color) == list:
+                fc_mid = color[align_index]
+                r, b, g = to_rgb(fc_mid)
+            else:
+                raise TypeError('color can only be dict or list')
+        else:
+            fc_mid = fc[align_index]
+            r, b, g = fc_mid
+
         rect = patches.Rectangle((mid_point, y), row[align_category],
-                                 facecolor=color[mid_index],
+                                 facecolor= fc_mid,
                                  **bar_kws)
         ax.add_patch(rect)
         y_center = y + 0.5 * bar_kws['height']
-        r,b,g = color[mid_index]
-        text_color = 'white' if r * g * b < 0.5 else 'darkgrey'
+        text_color = 'white' if r * g * b < 0.25 else 'black'
         bar_perc = 100 * row[align_category]/row.sum()
         if show_perc and bar_perc >= min_label_perc:
             ax.text(mid_point+row[align_category]/2, y_center,
                     '{:.0f}%'.format(bar_perc),
                     ha='center', va='center', color =text_color)
         for ind, column_name in enumerate(data.columns):
-            if ind == mid_index:
-                continue
-            elif ind < mid_index:
+            if ind == align_index:
+                if ind == 0:
+                    neg_x.append(mid_point-0.5)
+                    continue
+                elif ind == len(data.columns)-1:
+                    x = sum(row[x] for i, x in enumerate(data.columns) if
+                            i == ind) + mid_point
+                    pos_x.append(x + row[column_name])
+                    continue
+                else:
+                    continue
+            elif ind < align_index:
                 x = -sum(row[x] for i, x in enumerate(data.columns) if
-                         ind <= i < mid_index) + mid_point
+                         ind <= i < align_index) + mid_point
                 neg_x.append(x)
             else:
                 x = sum(row[x] for i, x in enumerate(data.columns) if
-                        mid_index <= i < ind) + mid_point
+                        align_index <= i < ind) + mid_point
                 pos_x.append(x + row[column_name])
 
+            if color is not None:
+                fc_i = color[column_name] if type(color) == dict else color[
+                    ind]
+                r, b, g = to_rgb(fc_mid)
+            else:
+                fc_i = fc[ind]
+                r, b, g = fc_i
             rect = patches.Rectangle((x, y), row[column_name],
-                                     facecolor=color[ind], **bar_kws)
+                                     facecolor=fc_i, **bar_kws)
             ax.add_patch(rect)
-            r, b, g = color[ind]
-            text_color = 'white' if r * g * b < 0.5 else 'darkgrey'
+            text_color = 'white' if r * g * b < 0.25 else 'black'
             bar_perc = 100 * row[column_name] / row.sum()
             if show_perc and bar_perc >= min_label_perc:
                 ax.text(x + row[column_name] / 2, y_center,
@@ -112,6 +151,7 @@ def plot_likert_scales(data: DataFrame, align_category: str,
 
         y_position.append(y_center)
         y = y - bar_kws['height']-0.2
+
 
     plt.xlim(min(neg_x) * 1.1, max(pos_x) * 1.1)
     plt.ylim(0.02, (max(y_position) + 0.5 * bar_kws['height']) * 1.05)
@@ -126,7 +166,12 @@ def plot_likert_scales(data: DataFrame, align_category: str,
 
     handles, labels = ax.get_legend_handles_labels()
     for ind, column_name in enumerate(data.columns):
-        patch = mpatches.Patch(color=color[ind], label=column_name)
+        if color is not None:
+            hc = color[column_name] if type(color) == dict else color[
+                ind]
+        else:
+            hc = fc[ind]
+        patch = mpatches.Patch(color=hc, label=column_name)
         handles.append(patch)
     ax.legend(handles=handles, ncol=data.shape[1], **legend_kws)
     return ax
